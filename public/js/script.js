76298df13730daa8ef67a8b2e962db6a32c2fecd
cganzon <crganzon@gmail.com>
2022-04-17 20:32:22 -0700

    removed the moon icon code

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,7 @@ const moonIcon = document.querySelector('.moon-icon');
 themeToggleBtn.addEventListener('click', () => {
     if(document.body.classList.contains('dark-theme')) {
         document.body.classList.remove('dark-theme');
-        moonIcon.style.fill = '#111517';
     } else {
         document.body.classList.add('dark-theme');
-        moonIcon.style.fill = '#fff';
     }
 });
