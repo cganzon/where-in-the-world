d6fd734799e2b09a3f3964f10b73095216405598
cganzon <crganzon@gmail.com>
2022-04-17 18:03:18 -0700

    created public folder for static assets and added static assets middleware

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-
+// EJS Setup
 app.set('view engine', 'ejs');
 app.set(express.static(path.join(__dirname, 'views')));
+
+// Static assets middleware
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
