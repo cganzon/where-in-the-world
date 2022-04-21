1fd23fac664ad7cf3580122ca7356f526fb448d5
cganzon <crganzon@gmail.com>
2022-04-20 20:15:58 -0700

    changed the api url to the v3 one and implemented filter all countries by region

--- index.js
+++ index.js
@@ -8,16 +8,30 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set(express.static(path.join(__dirname, 'views')));
 
+// Body parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Static assets middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    axios.get('https://restcountries.com/v2/all')
+    axios.get('https://restcountries.com/v3.1/all')
         .then(response => {
             const countries = response.data;
             res.render('home', { countries });
         })
         .catch(err => console.log(err));
+});
+
+app.get('/region', (req, res) => {
+    const { filter } = req.query;
+    axios.get(`https://restcountries.com/v3.1/region/${filter}`)
+        .then(response => {
+            // res.send(response.data)
+            const countries = response.data;
+            res.render('home', { countries});
+        });;
 });
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
