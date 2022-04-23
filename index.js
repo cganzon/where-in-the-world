1a927a331f58da7d89cb57ee92e418c1b06330e4
cganzon <crganzon@gmail.com>
2022-04-22 20:02:22 -0700

    created the country details page and have it render the appropriate details

--- index.js
+++ index.js
@@ -44,11 +44,41 @@ app.get('/search', (req, res, next) => {
         .catch(() => next());
 });
 
+const getBorderCountries = async borderCountries => {
+    const borderCountriesArr = [];
+    // Checking to see if the country.borders property exists for the country that is being viewed
+    if(borderCountries) {
+        for(let borderCountry of borderCountries) {
+            await axios.get(`https://restcountries.com/v2/alpha/${borderCountry}`)
+                .then(response => {
+                    borderCountriesArr.push(response.data);
+                })
+                .catch(err => console.log(err));
+        }
+        return borderCountriesArr;
+    };
+};
+
 app.get('/:countryCode', (req, res, next) => {
     const { countryCode } = req.params;
     axios.get(`https://restcountries.com/v2/alpha/${countryCode}`)
-        .then(response => {
-            res.send(response.data);
+        .then(async response => {
+            const country = response.data;
+            console.log(country);
+            let languages;
+            let currencies;
+            if(country.currencies) {
+                // Mapping the currency names into a new array and using that to created a comma separated list (if there are multiple currencies)
+                currencies = country.currencies.map(currency => currency.name).join(', ');
+            };
+            if(country.languages) {
+                // Mapping the language names into a new array and using that to created a comma separated list (if there are multiple languages)
+                languages = country.languages.map(language => language.name).join(', ');
+            };
+            // The country.borders array (if it exists) only contains the alpha3Code for each border country
+            // Therefore, the getBorderCountries function is used to display the actual name of each border country instead of the alpha3Code (better user experience)
+            const borderCountries = await getBorderCountries(country.borders);
+            res.render('countryDetails', { country, currencies, languages, borderCountries });
         })
         .catch(() => next());
 });
